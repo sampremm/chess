efcd9db1dd85e2fe78075d633ba9c70570a77e0d
sampremm <samprem888111@gmail.com>
2024-08-31 19:26:42 +0530

    updated_code

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,6 +1,14 @@
 const socket = io(); 
+const chess=new Chess();
+const boardElement=document.querySelector(".chessboard")
 
-socket.emit("game");
-socket.on("connectiontoall", ()=>{
-    console.log("connectedtoallXD")
-})
+let draggedpiece =null;
+let sourceSquare=null;
+let playerRole=null;
+
+const renderBoard=()=>{
+    const board=chess.boad();
+    
+};
+const handleMove=()=>{};
+const getPieceCode=()=>{};
