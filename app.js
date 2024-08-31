fef4f0af642cdd409078e3828d112dac4f7c9f84
sampremm <samprem888111@gmail.com>
2024-08-31 17:11:17 +0530

    Foundation_Server_http
    -created a server with express and connected http with an another server
    -setup the envinorment

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+const { Chess } = require("chess.js");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app); 
+const io = socket(server);
+
+const chess = new Chess();
+  
+let players = {};
+let currentPlayer = "w";
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+server.listen(3000, () => {
+    console.log("The server is listening on port 3000");
+});
